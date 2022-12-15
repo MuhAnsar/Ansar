1b64f283041354d0e788d33b889f43151bede4b5
zarcod9 <120603356+zarcod9@users.noreply.github.com>
2022-12-15 11:38:51 +0800

    Add files via upload

---
+++ membuat_perintah_delete.sql
@@ -0,0 +1,57 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 15.0
+-- Dumped by pg_dump version 15.0
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: mahasiswa; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.mahasiswa (
+    id integer NOT NULL,
+    nim character varying(20),
+    nama character varying(30),
+    alamat character varying(50)
+);
+
+
+ALTER TABLE public.mahasiswa OWNER TO postgres;
+
+--
+-- Data for Name: mahasiswa; Type: TABLE DATA; Schema: public; Owner: postgres
+--
+
+COPY public.mahasiswa (id, nim, nama, alamat) FROM stdin;
+1	0221107	Muh Ansar	Campalagian
+\.
+
+
+--
+-- Name: mahasiswa mahasiswa_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.mahasiswa
+    ADD CONSTRAINT mahasiswa_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
